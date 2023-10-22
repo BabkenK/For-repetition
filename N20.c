7df0527cc209f6dea79be4f0586b5ab8da9b1ab6
BabkenK <139691909+BabkenK@users.noreply.github.com>
2023-10-22 15:58:38 +0400

    20.Իրականացրեք ֆունկցիա, որը ստուգում է արդյոք տրված թիվը պարզ է։
    📚Իրականացրեք ֆունկցիա, որը ստուգում է արդյոք տրված թիվը պարզ է։

---
+++ N20.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n <= 1) {
+        return 0; // Not prime
+    }
+    
+    if (n <= 3) {
+        return 1; // Prime
+    }
+    
+    if (n % 2 == 0 || n % 3 == 0) {
+        return 0; // Not prime
+    }
+    
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return 0; // Not prime
+        }
+    }
+    
+    return 1; // Prime
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
